e6c41b33d2f526ec12cecea2234d054dea2fb316
ThiagoKagoiki <161661686+ThiagoKagoiki@users.noreply.github.com>
2025-12-09 19:40:17 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementacao do tabenews.com.br do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Melhorar não é opção, e sim uma tarefa! :)</h1>
+function Home() {
+  return <h1>Melhorar não é opção, e sim uma tarefa! :)</h1>;
+}
+
+function testePrettier() {
+  console.log("teste");
 }
 
 export default Home;
