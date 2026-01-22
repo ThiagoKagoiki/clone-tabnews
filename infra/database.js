a7cca6976b431f599b1bff24b84700d1e71e1f9c
ThiagoKagoiki <161661686+ThiagoKagoiki@users.noreply.github.com>
2026-01-22 21:44:00 +0000

    database.js mais robusto para erros

--- infra/database.js
+++ infra/database.js
@@ -8,11 +8,17 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const res = await client.query(queryObject);
-  await client.end();
 
-  return res;
+  await client.connect();
+
+  try {
+    const res = await client.query(queryObject);
+    return res;
+  } catch (err) {
+    console.error("Erro ao conectar ao banco de dados: ", err);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
